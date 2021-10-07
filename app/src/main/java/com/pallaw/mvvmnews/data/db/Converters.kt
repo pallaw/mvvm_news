c05d1b1293d2653ad255f85af04f2facff17326a
Pallaw Pathak <pallaw.pathak@shipsy.co.in>
2021-10-08 02:58:50 +0530

    Database and converters for non primitive data class adoption in room

---
+++ app/src/main/java/com/pallaw/mvvmnews/data/db/Converters.kt
@@ -0,0 +1,17 @@
+package com.pallaw.mvvmnews.data.db
+
+import androidx.room.TypeConverter
+import com.pallaw.mvvmnews.data.model.Source
+
+class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source): String {
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
+}
